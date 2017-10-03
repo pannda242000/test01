5cee80af5a4fa1ff0256f2e8ca85515201fe2a34
pannda242000 <32472519+pannda242000@users.noreply.github.com>
2017-10-03 10:58:28 +0900

    001
    commit01

---
+++ test_script.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+USERNAME='test01'
+PASSWORD='Test_180180'
+
+useradd -m -G wheel -s /bin/bash "${USERNAME}"
+echo "${PASSWORD}" | passwd --stdin "${USERNAME}"
+
+sed -e "s/PermitRootLogin yes/PermitRootLogin no/g" -i /etc/ssh/sshd_config
+sed -e "s/#PermitRootLogin yes/PermitRootLogin no/g" -i /etc/ssh/sshd_config
+sed -e "s/#PermitRootLogin no/PermitRootLogin no/g" -i /etc/ssh/sshd_config
+
+systemctl restart sshd
+
+mkdir -p /home/WORK/STARTUP/
